ec9b0acc534cb0d2bfdd46b7578cb1580b5726dd
Darius Napigkit <darius.napigkit@gmail.com>
2025-10-06 11:10:39 -0700

    Updated the root module to include adding of tags.

--- projects/liveprojects/secure-financial-transactions/main.tf
+++ projects/liveprojects/secure-financial-transactions/main.tf
@@ -12,13 +12,37 @@ data "aws_iam_policy_document" "ec2_assume_role" {
 }
 
 # ------------------------------------------------
+# MODULE CALL TO GENERATE STANDARD TAGS
+# ------------------------------------------------
+module "standard_tags" {
+  source = "git::https://github.com/darius-napigkit/nss-ips-common.git//terraform/modules/aws/tagging-standard?ref=develop" # Pointing to a GitHub repo and branch
+
+  environment = "Development"
+  project     = "Manning"
+
+  # Custom tags specific to this root configuration
+  additional_tags = {
+    Name       = "liveproject"
+    Cluster    = "None"
+    Creator    = "Terraform"
+    Expires    = "2025-12-01T00:00:00+00:00"
+    Service    = "Compliance"
+    Management = "dnapigkit@gmail.com"
+    Freetext   = "Account for liveproject: Mastering Compliance with Ansible Terraform and OpenSCAP"
+  }
+}
+
+# ------------------------------------------------
 # LiveProject Role for a power user (FULL ACCESS USER CREATED)
 # ------------------------------------------------
 module "liveproject_role" {
-  source = "git::ssh://git@github.com/darius-napigkit/nss-ips-common.git//terraform/modules/aws/iam-role?ref=develop" # Pointing to a GitHub repo and branch
+  source = "git::https://github.com/darius-napigkit/nss-ips-common.git//terraform/modules/aws/iam-role?ref=develop" # Pointing to a GitHub repo and branch
 
   role_name          = "LiveProject-Role"
   assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+
+  # Pass the tags output from the tagging module
+  tags = module.standard_tags.tags
 
   # Attach multiple policies
   policy_arns = [
